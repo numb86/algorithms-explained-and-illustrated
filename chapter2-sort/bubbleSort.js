f74b005262690b5a90ee63ffb6b615e10217fc3a
numb86 <numb86@users.noreply.github.com>
2019-09-20 15:28:20 +0900

    バブルソートを実装した。

---
+++ chapter2-sort/bubbleSort.js
@@ -0,0 +1,26 @@
+function main(input) {
+  function compare(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const left = array[i - 1];
+      const right = array[i];
+      if (left <= right) continue;
+      array[i] = left;
+      array[i - 1] = right;
+    }
+    return array;
+  }
+
+  function sort(decision, rest) {
+    if (rest.length <= 1) return decision.concat(rest);
+    const comparedArray = compare(rest);
+    decision.push(comparedArray[0]);
+    comparedArray.shift();
+    return sort(decision, comparedArray);
+  }
+
+  return sort([], input);
+}
+
+const target = [1, 5, 6, 1, 7, 6, 2, 3];
+console.log(`<< ${target}`);
+console.log(`>> ${main(target)}`);
