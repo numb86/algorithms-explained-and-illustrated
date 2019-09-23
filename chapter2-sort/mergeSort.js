5f0b647b880addff60563b8a3d30ff3ae10351c1
numb86 <16703337+numb86@users.noreply.github.com>
2019-09-23 15:08:32 +0900

    マージソートを実装した。

---
+++ chapter2-sort/mergeSort.js
@@ -0,0 +1,44 @@
+function main(input) {
+  function merge(left, right) {
+    const result = [];
+    while(left.length !== 0 || right.length !== 0) {
+      if (left.length === 0) {
+        result.push(right[0]);
+        right.shift();
+        continue;
+      }
+      if (right.length === 0) {
+        result.push(left[0]);
+        left.shift();
+        continue;
+      }      
+      if (left[0] <= right[0]) {
+        result.push(left[0]);
+        left.shift();
+        continue;
+      }
+      result.push(right[0]);
+      right.shift();
+    }
+    return result;
+  }
+
+  function loop(container) {
+    const processedContainer = [];
+    for (let i = 0; i < container.length - 1; i += 2) {
+      processedContainer.push(merge(container[i], container[i + 1]));
+    }
+    if (container.length % 2 === 1) {
+      processedContainer.push(container[container.length - 1]);
+    }
+
+    if (processedContainer.length === 1) return processedContainer[0];
+    return loop(processedContainer);
+  }
+
+  return loop(input.map(elem => [elem]));
+}
+
+const target = [1, 5, 6, 1, 7, 6, 2, 3];
+console.log(`<< ${target}`);
+console.log(`>> ${main(target)}`);
